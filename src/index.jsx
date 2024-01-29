8b0bd085bbce5936a27410246f83eda8f14ab281
nishantjangid <nishantsharma39262@gmail.com>
2024-01-29 10:19:41 +0530

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import "./i18n";
+import reportWebVitals from "./reportWebVitals";
+import { wagmiConfig } from './walletConfiguration/Config';
+import { WagmiConfig} from 'wagmi'
+
+const Root = () => {
+  return (
+    <div className="App">
+       <WagmiConfig config={wagmiConfig}>
+        <App />
+       </WagmiConfig>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Root />);
+
+// ReactDOM.createRoot(<App />, document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
