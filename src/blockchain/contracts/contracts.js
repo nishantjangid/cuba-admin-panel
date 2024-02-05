450269026b4bf2f117606ba411931b3ebb7e08cf
ThinkerTechnology <defichainnfts@gmx.de>
2024-02-05 15:52:00 +0530

    fixed: change smart contract

--- src/blockchain/contracts/contracts.js
+++ src/blockchain/contracts/contracts.js
@@ -1,2 +1,2 @@
-export const testContractAddress = "0x017F8a2da333a4c47E3f1A8db8d937AC155D3d97";
-export const testBUSDcontractAddress = "0x2be1aff54a32371c9af351d9637b710aa81b33fc";
+export const testContractAddress = "0x5c32f02E792fF15F32b569487bBb09Ea1f16452B";
+export const testBUSDcontractAddress = "0xeB31705512137a0F90010AF1131A562998B1d190";
