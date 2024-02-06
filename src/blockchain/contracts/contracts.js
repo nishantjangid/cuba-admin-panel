d797f96091c37d2ed47807a9db6d07d85d45cb20
ThinkerTechnology <defichainnfts@gmx.de>
2024-02-06 10:55:33 +0530

    fixed:smart contract Address

--- src/blockchain/contracts/contracts.js
+++ src/blockchain/contracts/contracts.js
@@ -1,2 +1,2 @@
-export const testContractAddress = "0x5c32f02E792fF15F32b569487bBb09Ea1f16452B";
+export const testContractAddress = "0x26609F3e4A3019AB06BAC093b000130Ea742cb8b";
 export const testBUSDcontractAddress = "0xeB31705512137a0F90010AF1131A562998B1d190";
